4bd5a32c871740356a5c1f3425392dd3c4290259
Tymofii Lanevych <timlan310398@gmail.com>
2019-08-28 15:04:55 +0300

    front: add new message

--- src/main/java/com/timak31/letscoderestvue/RestVueApplication.java
+++ src/main/java/com/timak31/letscoderestvue/RestVueApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LetscodeRestVueApplication {
+public class RestVueApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(LetscodeRestVueApplication.class, args);
+        SpringApplication.run(RestVueApplication.class, args);
     }
 
 }
